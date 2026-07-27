3d2a60bc42f8d66efabb1423bc504c8c114bd1a0
antonio <cruisematch@gmail.com>
2026-07-28 01:19:00 +0200

    Navbar mobile

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,11 @@
 "use client";
-
+import { useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function Navbar() {
   const pathname = usePathname();
-
+const [open, setOpen] = useState(false);
  const linkStyle = (path: string) =>
   `px-5 py-2 rounded-xl transition-all duration-300 border border-white/30 ${
     pathname === path
@@ -15,49 +15,70 @@ export default function Navbar() {
 
   return (
    <header className="fixed top-0 left-0 right-0 z-50 backdrop-blur-xl bg-slate-950/80 border-b border-white/20 shadow-lg">
-      <div className="max-w-7xl mx-auto flex items-center justify-between px-8 py-4">
+      <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between px-4 md:px-8 py-4">
 
-        <Link
-          href="/"
-          className="text-3xl font-black tracking-wide text-cyan-300"
-        >
-          🚢 CruiseMatch
-        </Link>
+       
+ <Link
+  href="/"
+  className="text-2xl md:text-3xl font-black tracking-wide text-cyan-300"
+>
+  🚢 CruiseMatch
+</Link>
 
-        <nav className="flex items-center gap-3">
+       <nav className="flex items-center">
 
-          <Link href="/" className={linkStyle("/")}>
-            Home
-          </Link>
+<button
+  onClick={() => setOpen(!open)}
+  className="md:hidden text-white text-3xl"
+>
+  ☰
+</button>
 
-          <Link href="/community" className={linkStyle("/community")}>
-            Community
-          </Link>
+<div className={`
+  ${open ? "flex" : "hidden"}
+  md:flex
+  absolute md:static
+  top-20 left-0 right-0
+  flex-col md:flex-row
+  bg-slate-950/95
+  p-6 md:p-0
+  gap-3
+`}>
 
-          <Link href="/cruises" className={linkStyle("/cruises")}>
-            Crociere
-          </Link>
+<Link href="/" className={linkStyle("/")}>
+Home
+</Link>
 
-          <Link href="/my-cruise" className={linkStyle("/my-cruise")}>
-            La mia crociera
-          </Link>
+<Link href="/community" className={linkStyle("/community")}>
+Community
+</Link>
 
-          <Link href="/profile" className={linkStyle("/profile")}>
-  Profilo
+<Link href="/cruises" className={linkStyle("/cruises")}>
+Crociere
+</Link>
+
+<Link href="/my-cruise" className={linkStyle("/my-cruise")}>
+La mia crociera
+</Link>
+
+<Link href="/profile" className={linkStyle("/profile")}>
+Profilo
 </Link>
 
 <Link href="/my-chats" className={linkStyle("/my-chats")}>
-  Le mie chat
+Le mie chat
 </Link>
 
 <Link
-  href="/login"
-            className="ml-4 bg-cyan-400 text-slate-900 px-5 py-2 rounded-xl font-bold hover:scale-105 transition"
-          >
-            Accedi
-          </Link>
+href="/login"
+className="bg-cyan-400 text-slate-900 px-5 py-2 rounded-xl font-bold"
+>
+Accedi
+</Link>
 
-        </nav>
+</div>
+
+</nav>
       </div>
     </header>
   );
